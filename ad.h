c68bbd5678485055b08fa993131f3466760836b3
Javier Francisco Díaz <alu0101049021@ull.edu.es>
2025-11-08 19:52:22 +0000

    Cambiados tipos incompatibles

--- ad.h
+++ ad.h
@@ -17,33 +17,33 @@
 #include <sys/locks.h>
 
 // elegir puerto de conversion
-bool ad_conf_puerto(uint8_t);
+uint8_t ad_conf_puerto(uint8_t);
 
 // configurar conversión de 8 o 10 bits
-bool ad_conf_bits(uint8_t);
+uint8_t ad_conf_bits(uint8_t);
 
 // configurar tiempo de muestreo
-bool ad_conf_tiempo_muestreo(uint8_t);
+uint8_t ad_conf_tiempo_muestreo(uint8_t);
 
 // configurar numero de conversiones sucesivas (1, 4 u 8)
-bool ad_conf_num_conversiones(uint8_t);
+uint8_t ad_conf_num_conversiones(uint8_t);
 
 // configurar modo de lectura: único pin, pines sucesivos 
-bool ad_activar_lectura_pines_sucesivos(uint8_t);
+uint8_t ad_activar_lectura_pines_sucesivos(uint8_t);
 
 // configurar pin de inicio
-bool ad_conf_pin_inicio(uint8_t);
+uint8_t ad_conf_pin_inicio(uint8_t);
 
 // configurar el modo de conversión continua (SCAN)
-bool ad_activa_lectura_continua(uint8_t);
+uint8_t ad_activa_lectura_continua(uint8_t);
 
 // iniciar la conversión
-bool ad_inicia_conversion();
+uint8_t ad_inicia_conversion();
 
 // esperar a que termine conversión
-bool ad_espera_conversion();
+uint8_t ad_espera_conversion();
 
 // devolver los valores leídos
-bool ad_recupera_conversion(uint16_t*);
+uint8_t ad_recupera_conversion(uint16_t*);
 
 // instalar función manejadora para cuando termine conversión
