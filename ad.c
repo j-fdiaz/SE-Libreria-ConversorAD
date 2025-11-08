1767fd1d0f687749432c01730c8dba4cb8b449a9
Javier Francisco Díaz <alu0101049021@ull.edu.es>
2025-11-08 19:50:36 +0000

    Cambiados tipos incompatibles

--- ad.c
+++ ad.c
@@ -16,108 +16,111 @@
 #include <sys/sio.h>
 #include <sys/locks.h>
 
+#define TRUE 1
+#define FALSE 0
+
 uint8_t mi_CTL3 = 0;
 uint8_t mi_CTL4 = 0;
 uint8_t mi_CTL5 = 0;
 uint8_t dir_ad = 0;
 
 // elegir puerto de conversion
-bool ad_conf_puerto(uint8_t puerto) {
+uint8_t ad_conf_puerto(uint8_t puerto) {
   dir_ad = (puerto == 0)?0:(M6812_ATD1CTL0 - M6812_ATD0CTL0);
-  return true;
+  return TRUE;
 }
 
 // configurar conversión de 8 o 10 bits
-bool ad_conf_bits(uint8_t bits_conversion) { 
+uint8_t ad_conf_bits(uint8_t bits_conversion) {
   if (bits_conversion == 8) {
     // Desactivamos el bit RES10
     mi_CTL4 &= ~M6812B_RES10;
-    return true;
+    return TRUE;
   } else if (bits_conversion == 10) {
     // Activamos el bit RES10
     mi_CTL4 |= M6812B_RES10;
-    return true;
+    return TRUE;
   } else 
-    return false;
+    return FALSE;
 }
 
 // configurar tiempo de muestreo
-bool ad_conf_tiempo_muestreo(uint8_t tiempo_muestreo) {
+uint8_t ad_conf_tiempo_muestreo(uint8_t tiempo_muestreo) {
   switch (tiempo_muestreo) {
   case 2:
     mi_CTL4 &= ~M6812B_SMP1 & ~M6812B_SMP0;
-    return true;
+    return TRUE;
   case 4:
     mi_CTL4 &= ~M6812B_SMP1;
     mi_CTL4 |= M6812B_SMP0;
-    return true;
+    return TRUE;
   case 8:
     mi_CTL4 |= M6812B_SMP1;
     mi_CTL4 &= ~M6812B_SMP0;
-    return true;
+    return TRUE;
   case 16:
     mi_CTL4 |= M6812B_SMP1 | M6812B_SMP0;
-    return true;
+    return TRUE;
   default:
-    return false;
+    return FALSE;
   }
 }
 
 // configurar numero de conversiones sucesivas
-bool ad_conf_num_conversiones(uint8_t conversiones) {
+uint8_t ad_conf_num_conversiones(uint8_t conversiones) {
   switch (conversiones) {
   case 1:
     mi_CTL5 &= ~M6812B_S8C;
     mi_CTL3 |= M6812B_S1C;
-    return true;
+    return TRUE;
   case 4:
     mi_CTL5 &= ~M6812B_S8C;
     mi_CTL3 &= ~M6812B_S1C;
-    return true;
+    return TRUE;
   case 8:
     mi_CTL5 |= M6812B_S8C;
-    return true;
+    return TRUE;
   default:
-    return false;
+    return FALSE;
   }
 }
 
 // configurar modo de lectura: único pin, pines sucesivos
-bool ad_activar_lectura_pines_sucesivos(uint8_t modo) {
+uint8_t ad_activar_lectura_pines_sucesivos(uint8_t modo) {
   if (modo == 0)
     mi_CTL5 &= ~M6812B_MULT;
   else
     mi_CTL5 |= M6812B_MULT;
-  return true;
+  return TRUE;
 }
 
 // configurar pin de inicio
-bool ad_conf_pin_inicio(uint8_t canal) {
+uint8_t ad_conf_pin_inicio(uint8_t canal) {
   mi_CTL5 &= ~(0x07);
   mi_CTL5 |= canal & (0x07);
-  return true;
+  return TRUE;
 }
 
 // configurar el modo de conversión continua (SCAN)
-bool ad_activa_lectura_continua(uint8_t modo) {
+uint8_t ad_activa_lectura_continua(uint8_t modo) {
   if (modo == 0)
     mi_CTL5 &= ~M6812B_SCAN;
   else
     mi_CTL5 |= M6812B_SCAN;
-  return true;
+  return TRUE;
 }
 
 // iniciar la conversión
-bool ad_inicia_conversion();
+uint8_t ad_inicia_conversion();
 
 // esperar a que termine conversión
-bool ad_espera_conversion();
+uint8_t ad_espera_conversion();
 
 // devolver los valores leídos
-bool ad_recupera_conversion(uint16_t* );
+uint8_t ad_recupera_conversion(uint16_t* );
 
 // instalar función manejadora para cuando termine conversión
 //  Función que recibe un puntero a una función, tiene que activar
 //  interrupciones. Solo se ejecuta la función recibida cuando 
 //  recibe la interrupción (al terminar la conversión)
-bool 
+uint8_t 
